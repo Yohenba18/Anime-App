b8bfa0de4e4b72233142367d950448b4c454e174
Yohenba18 <roddik12kshetri@gmail.com>
2021-04-19 00:23:11 +0530

    pages

---
+++ src/pages/moreinfo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function moreinfo() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default moreinfo
---
+++ src/pages/welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function welcome() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default welcome
