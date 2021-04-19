2eb7ab05e93dcc8c34829ed9a8a4263018387e18
Yohenba18 <roddik12kshetri@gmail.com>
2021-04-19 21:00:25 +0530

    git welcome v1.2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Welcome from './pages/welcome';
+import Result from './pages/result';
+import Moreinfo from './pages/moreinfo';
 
 function App() {
   return (
     <div className="App">
       <Welcome/>
+
     </div>
   );
 }
