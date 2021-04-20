ae53ce8dbc68eccf6b7a3a74b9406ec3c12926aa
Yohenba18 <roddik12kshetri@gmail.com>
2021-04-20 21:08:05 +0530

    welcome page search v1.2

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,10 +1,12 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { SearchContext } from "../context/search";
 import "./welcome.scss";
-import {Grid, FormControl, Input, IconButton} from "@material-ui/core"
+import { Grid, FormControl, Input, IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Welcome = () => {
   const search = useContext(SearchContext);
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     search.search("Naruto").then((data) => console.log(data));
@@ -14,9 +16,31 @@ const Welcome = () => {
     <div className="welcome">
       <div className="welcome__elements">
         <h1>WEEB ZONE</h1>
-        <Grid container direction="column" justify="center" alignContent = "center" alignItem="center">
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignContent="center"
+          alignItem="center"
+        >
           <Grid item>
-          Search
+            <form>
+              <FormControl type="submit">
+                <Input
+                  placeholder="Anime search...."
+                  value={input}
+                  onClick={}
+                />
+                <IconButton
+                  varient="contained"
+                  type="submit"
+                  disable={!input}
+                  onClick={handleSearch}
+                >
+                  <SearchIcon />
+                </IconButton>
+              </FormControl>
+            </form>
           </Grid>
         </Grid>
       </div>
