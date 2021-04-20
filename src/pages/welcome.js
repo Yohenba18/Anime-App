1bb22bc2bead82c6f798b2c2f2ced92d92015880
Yohenba18 <roddik12kshetri@gmail.com>
2021-04-20 21:02:05 +0530

    grid

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { SearchContext } from "../context/search";
 import "./welcome.scss";
+import {Grid, FormControl, Input, IconButton} from "@material-ui/core"
 
 const Welcome = () => {
   const search = useContext(SearchContext);
@@ -13,6 +14,11 @@ const Welcome = () => {
     <div className="welcome">
       <div className="welcome__elements">
         <h1>WEEB ZONE</h1>
+        <Grid container direction="column" justify="center" alignContent = "center" alignItem="center">
+          <Grid item>
+          Search
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
