e31d4ba5fe9cd577919e57458518bac80259e60b
Yohenba18 <roddik12kshetri@gmail.com>
2021-04-24 00:06:47 +0530

    nav v2.5

--- src/components/Topnav.js
+++ src/components/Topnav.js
@@ -1,13 +1,13 @@
-import React, { useContext, useEffect, useState } from 'react';
-import './TopNav.scss'
-;import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
-import { useHistory } from 'react-router';
-import { SearchContext } from '../context/search';
+import React, { useContext, useEffect, useState } from "react";
+import "./TopNav.scss";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import { useHistory } from "react-router";
+import { SearchContext } from "../context/search";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,47 +15,47 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
     },
   },
   search: {
-    position: 'relative',
+    position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
+    "&:hover": {
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(1),
-      width: 'auto',
+      width: "auto",
     },
   },
   searchIcon: {
     padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   inputRoot: {
-    color: 'inherit',
+    color: "inherit",
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
       },
     },
   },
@@ -64,14 +64,19 @@ const useStyles = makeStyles((theme) => ({
 export default function SearchAppBar() {
   const classes = useStyles();
   const history = useHistory();
-  const search  = useContext(SearchContext);
-  const [input,setInput] = useState(true);
+  const search = useContext(SearchContext);
+  const [input, setInput] = useState(true);
 
-  useEffect(() => {
-    
-    history.push('//Result')
-  })
+  const handleSearch = (event) => {
+    event.preventDefault();
+    search.search(input).then((data) => {
+      search.setData(data.resutls);
+      localStorage.setItem('myData',JSON.stringify(data.resutls));
+      setInput(' ');
+      history.push('/Result');
+    });
 
+  };
 
   return (
     <div className={classes.root}>
@@ -80,7 +85,7 @@ export default function SearchAppBar() {
           <Typography className={classes.title} variant="h4" noWrap id="title">
             <a href="../pages/welcome.js">WEEB ZONE</a>
           </Typography>
-          <div className={classes.search}>
+          <form className={classes.search} onSubmit={handleSearch}>
             <div className={classes.searchIcon}>
               <SearchIcon />
             </div>
@@ -90,11 +95,13 @@ export default function SearchAppBar() {
                 root: classes.inputRoot,
                 input: classes.inputInput,
               }}
-              inputProps={{ 'aria-label': 'search' }}
+              inputProps={{ "aria-label": "search" }}
               value={input}
-              onChange={(e) => {setInput(e.target.value)}}
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
             />
-          </div>
+          </form>
         </Toolbar>
       </AppBar>
     </div>
