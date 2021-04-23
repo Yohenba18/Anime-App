222720368d98cf14ddbef876dfb18edfa79bbfe8
Yohenba18 <roddik12kshetri@gmail.com>
2021-04-24 00:01:29 +0530

    nav v2.1

--- src/components/Topnav.js
+++ src/components/Topnav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './TopNav.scss'
 ;import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,6 +6,8 @@ import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
+import { useHistory } from 'react-router';
+import { SearchContext } from '../context/search';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -61,6 +63,14 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SearchAppBar() {
   const classes = useStyles();
+  const history = useHistory();
+  const search  = useContext(SearchContext);
+  const [input,setInput] = useState(true);
+
+  useEffect(() => {
+    history
+  })
+
 
   return (
     <div className={classes.root}>
