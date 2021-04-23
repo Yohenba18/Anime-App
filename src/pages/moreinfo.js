cc424018302ea140411424df9fcd5914d4250882
Yohenba18 <roddik12kshetri@gmail.com>
2021-04-23 21:17:17 +0530

    moreinfo v1.1

--- src/pages/moreinfo.js
+++ src/pages/moreinfo.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import { SearchContext } from '../context/search';
 
 function Moreinfo() {
+
+    const search = useContext(SearchContext);
+    const [dataExist, setDataExist] = useState(true);
+
+    useEffect(() => {
+        if(search.singleData === undefined ||  Object.keys(search.singleData).length === 0){
+            try{
+                search.setSingle(JSON.parae(localStorage.getItem('singleData')))
+                setDataExist(true);
+            }catch(e){
+                console.log(e);
+                setDataExist(false)
+            } 
+        }
+        console.log(search);
+    },[search])
     return (
         <div>
-            <h1>More Info</h1>
+            {dataExist ? (<h1>More Info</h1>) : (<h1>nope</h1>)}
         </div>
     )
 }
